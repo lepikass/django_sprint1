976762e05e1c31bc6aa013c9992a13331d011d15
lepikass <lepikass@icloud.com>
2024-08-22 10:56:35 -0700

    1

---
+++ blogicum/blogicum/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.shortcuts import redirect
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(('blog.urls', 'blog'), namespace='blog')), 
+    path('pages/', include(('pages.urls', 'pages'), namespace='pages')),
+    re_path(r'^.*$', lambda request: redirect('/')),
+]
